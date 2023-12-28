9f9ea3970daa546673faf7c22a6c8d36093a0bae
tireofthisplace <cristalaiz1337@gmail.com>
2023-12-28 18:53:25 +0200

    fix1

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,6 +1,7 @@
 const {Brand, Type} = require('../models/models');
 const ApiError = require('../error/ApiError');
 
+
 class BrandController {
     async create (req, res) {
         const {name} = req.body
